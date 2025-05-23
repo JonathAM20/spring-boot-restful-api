90c103f77911f9eb22df63384c3ddd95d0c9fb80
Jonathan Sousa <jonathan_msi@live.com>
2025-05-23 14:44:17 +0100

    Lombok dependency import and usage

--- src/main/java/com/project/springboot_restful_api/model/Role.java
+++ src/main/java/com/project/springboot_restful_api/model/Role.java
@@ -2,6 +2,15 @@ package com.project.springboot_restful_api.model;
 
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
 
     private Long id;
