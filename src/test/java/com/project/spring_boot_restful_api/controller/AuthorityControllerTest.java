3404104b261871a48f183d32eda7037d1d550dac
Jonathan Sousa <jonathan_msi@live.com>
2025-05-27 22:54:18 +0100

    Basic Authority Controller Unit Tests

---
+++ src/test/java/com/project/spring_boot_restful_api/controller/AuthorityControllerTest.java
@@ -0,0 +1,66 @@
+package com.project.spring_boot_restful_api.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.project.spring_boot_restful_api.model.Authority;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthorityControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testDeleteById() throws Exception {
+        mockMvc.perform(delete("/authority").param("id", "1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testFindAll() throws Exception {
+        mockMvc.perform(get("/authority"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testFindByName() throws Exception {
+        mockMvc.perform(get("/authority/teste"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testSave() throws Exception {
+        Authority authority = Authority.builder().name("teste").build();
+
+        mockMvc.perform(post("/authority")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(authority)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        Authority authority = Authority.builder().name("teste2").build();
+
+        mockMvc.perform(put("/authority/teste")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(authority)))
+                .andExpect(status().isBadRequest());
+    }
+}
